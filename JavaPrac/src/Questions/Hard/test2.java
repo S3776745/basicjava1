598d3372c9dad5fd94d5cf870505acae7230a380
s3776745 <s3776745@student.rmit.edu.au>
2021-11-13 23:30:10 +0800

    JAVA tests

--- JavaPrac/src/Questions/Hard/test2.java
+++ JavaPrac/src/Questions/Hard/test2.java
@@ -3,7 +3,7 @@ package Questions.Hard;
 public class test2 {
 
 /*  Define an int array of length 6,
-    Let the user enter six arrays greater than zero at once (separated by "0")
+    Let the user enter six numbers greater than zero at once (separated by "0")
     Use try & catch, if the user enters something that's not number,
     Show that only numbers can be entered, allowing the user to re-enter.
     Fill the array with the six values entered by the user.
